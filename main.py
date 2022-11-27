576726db60f11873b0d52b049060f0126cfe428b
dolbilov <dolbilovkonstantin@gmail.com>
2022-11-28 00:25:47 +1000

    Убрал лишние строки кода

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify, redirect
+from flask import Flask, request, render_template, redirect
 from utils import get_posts_all, get_post_by_pk, get_comments_by_post_id, search_for_posts, get_posts_by_user
 import json
 from api.api import api_blueprint
@@ -110,7 +110,7 @@ def remove_to_bookmarks(postid):
     with open('data/bookmarks.json', 'w', encoding='utf-8') as file:
         json.dump(bookmarks, file)
 
-    return redirect("/", code = 302)
+    return redirect("/", code=302)
 
 
 @app.route("/bookmarks", methods=['GET'])
@@ -152,33 +152,6 @@ def page_not_found(e):
     дизайном, а так же явно устанавливаем статус 500
     """
     return render_template('500.html'), 500
-
-
-# @app.route("/api/posts", methods=['GET'])
-# def return_posts_by_json():
-#     """
-#     Возвращает список JSON-словаря со всеми постами
-#     :return: список со словарем
-#     """
-#     posts = get_posts_all()
-#     logging.basicConfig(level=logging.INFO, filename='logs/api.log', format='%(asctime)s [%(levelname)s] %(message)s')
-#     logging.info('Запрос api/posts')
-#     return jsonify(posts)
-
-
-# @app.route("/api/posts/<int:postid>", methods=['GET'])
-# def return_post_by_json(postid):
-#     """
-#     Возвращает JSON-словарь с одним постом по заданному postid
-#     :param postid: Номер поста
-#     :return: JSON-словарь
-#     """
-#     post = get_post_by_pk(postid)
-#     path = f'api/posts/{postid}'
-#     logging.basicConfig(level=logging.INFO, filename='logs/api.log', format='%(asctime)s [%(levelname)s] %(message)s')
-#     logging.info(f'Запрос {path}')
-#     return jsonify(post)
-
 
 
 app.run(host='0.0.0.0', port=800)
